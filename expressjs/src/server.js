3f31d98f04272c2ac211c74fd696ee5dd013569f
Chris Brouwer <chris.brouwer@gmail.com>
2018-07-12 20:54:57 +0200

    use stuff

--- expressjs/src/server.js
+++ expressjs/src/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 const bodyParser= require('body-parser');
 const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient,
-      Server = require('mongodb').Server
+      Server = require('mongodb').Server,
+      ObjectID = require('mongodb').ObjectID
 
 var app = express();
 app.use( bodyParser.json() );
@@ -30,10 +31,21 @@ app.get('/_app/stuff', (req, res) => {
   })
 })
 
+app.delete('/_app/stuff', (req, res) => {
+  const id = req.query.id;
+  const collection = db.collection('Stuff');
+  collection.deleteOne({_id: new ObjectID(id)}, (err, obj) => {
+    assert.equal(err, null);
+    res.send({status: 'ok'});
+  });
+  
+  
+})
+
 app.post('/_app/stuff', (req, res) => {
   const data = req.body;
   const productName = data.name;
-  const cleanProductName = data.name.toLowerCase().trim();
+  const cleanProductName = data.name.toLowerCase().trim().split(' ');
   const productColl = db.collection('Product');
   productColl.findOne({Name: productName, CleanName: cleanProductName}, (err, result) => {
     assert.equal(err, null);
--- react/src/ListStuff.jsx
+++ react/src/ListStuff.jsx
@@ -15,11 +15,23 @@ class ListStuff extends Component {
 
 
   getProducts() {
-    var context = this;
+    const context = this;
     axios.get('/_app/stuff')
     .then(function (response) {
       response.data.forEach(e=> e.Expires = moment(e.Expires) )
       context.setState({products: response.data})
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+  }
+
+  useProduct(id) {
+    const context = this;
+    axios.delete('/_app/stuff?id='+id)
+    .then(function (response) {
+      context.getProducts();
     })
     .catch(function (error) {
       // handle error
@@ -48,8 +60,7 @@ class ListStuff extends Component {
                 <td>{e.NrItems } </td>
                 <td>{e.Expires.format("dddd, MMMM Do YYYY") } </td>
                 <td>
-                  <Button className="ButtonOps"> Used </Button>
-                  <Button className="ButtonOps"> Moved </Button>
+                  <Button className="ButtonOps" onClick={() => this.useProduct(e._id)}> Used </Button>
                 </td>
               </tr>);
           })}
